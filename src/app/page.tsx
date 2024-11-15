30adf1f9b5d862c5119076fb2f68ad545bd5fabe
Gabriel Palhares <palhares@discente.ufg.br>
2024-11-15 18:06:34 -0300

    pagina login feita

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,34 @@
-import Image from "next/image";
+// src/pages/login.tsx
+import Image from 'next/image';
 
-export default function Home() {
+export default function Login() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex h-screen">
+      <div className="flex flex-col justify-center items-center w-1/2 bg-[#f2f2f2] p-10">
+        <Image src="/apogeu-logo.png" alt="Apogeu Brand Book Logo" width={180} height={150}/>
+        <h1 className="text-center mt-8 mb-12 text-2xl font-bold">Acesse, estude e transforme suas ideias!</h1>
+        <form className="flex flex-col gap-4 w-3/4 mt-6">
+          <label htmlFor="login" className="text-md font-bold ">Login</label>
+          <input type="text" id="login" className="p-2 rounded-3xl border shadow-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 border-r-2 bg-[#D9D8D8] h-[4rem] mb-3" />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <label htmlFor="password" className="text-md font-bold">Senha</label>
+          <input type="password" id="password" className="p-2 border shadow-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 bg-[#D9D8D8] h-[4rem] rounded-3xl" />
+
+          <div className="flex justify-between text-lg mt-2 ml-4">
+            <label className="flex items-center">
+              <input type="checkbox" className="mr-1 bg-[#D9D8D8] font-bold" />
+              Lembrar
+            </label>
+            <a href="#" className="text-black-500 underline">Esqueci minha senha</a>
+          </div>
+
+          <button type="submit" className="mt-20 p-3 bg-[#405F73] border  border-gray-300 text-white hover:bg-blue-600  h-[4rem] rounded-3xl text-4xl">Acessar</button>
+        </form>
+      </div>
+
+      <div className="relative w-1/2 bg-black">
+        <Image src="/direita-login.png" alt="Climbers on a mountain" layout="fill" objectFit="cover" />
+      </div>
     </div>
   );
 }
