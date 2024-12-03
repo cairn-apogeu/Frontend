0dc671af0b7adeceed762748a1da7b9ee8cf0b31
JOTAPEnvz <jpmoraistomaz01@gmail.com>
2024-12-03 16:00:22 -0300

    feat: criar estrutura da pagina principal

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useAuth } from "@clerk/clerk-react";
+import { useEffect } from "react";
+
+
+
+
+export default function InitialPage() {
+  const { isSignedIn, userId} = useAuth();
+  
+  useEffect(() => {
+    if (isSignedIn) {
+      console.log(`Usuário logado: ${userId}`);
+      document.cookie = `user=${userId}; path=/; max-age=86400; secure; samesite=strict`;
+
+    }
+  }, [isSignedIn, userId]);
+  return (
+    <div className="flex h-screen bg-[#141414]">
+
+        <div>
+          <button
+            type="submit"
+            className="mt-5 py-3 px-10 bg-[#4DB8FF] text-white hover:bg-[#0070bb] font-fustat font-semibold transition-all rounded-xl text-lg"
+          >
+          Login
+          </button>
+        </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,16 @@
-// src/app/login/page.tsx
-'use client';
-
-import { useAuth } from "@clerk/clerk-react";
-import { useEffect } from "react";
-
-
-
+"use client";
 
 export default function InitialPage() {
-  const { isSignedIn, userId} = useAuth();
-  
-  useEffect(() => {
-    if (isSignedIn) {
-      console.log(`Usuário logado: ${userId}`);
-      document.cookie = `user=${userId}; path=/; max-age=86400; secure; samesite=strict`;
-
-    }
-  }, [isSignedIn, userId]);
   return (
     <div className="flex h-screen bg-[#141414]">
-
-        <div>
-          <button
-            type="submit"
-            className="mt-5 py-3 px-10 bg-[#4DB8FF] text-white hover:bg-[#0070bb] font-fustat font-semibold transition-all rounded-xl text-lg"
-          >
+      <div>
+        <button
+          type="submit"
+          className="mt-5 py-3 px-10 bg-[#4DB8FF] text-white hover:bg-[#0070bb] font-fustat font-semibold transition-all rounded-xl text-lg"
+        >
           Login
-          </button>
-        </div>
+        </button>
+      </div>
     </div>
   );
 }
