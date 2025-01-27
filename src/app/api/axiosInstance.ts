9eff5a9a5847802f5804cd3af0753e55426a8c93
paulooctavioINTELI <124174379+paulooctavioINTELI@users.noreply.github.com>
2025-01-27 18:05:17 -0300

    fix: update axios instance

--- src/app/api/axiosInstance.ts
+++ src/app/api/axiosInstance.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3333', 
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, 
 });
 
 export default axiosInstance;
