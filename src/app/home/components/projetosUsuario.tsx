37c2a337ab94e04422ba294e0f3f95b722ca663d
paulooctavioINTELI <124174379+paulooctavioINTELI@users.noreply.github.com>
2025-02-05 11:24:44 -0300

    fix: test /projetos route

--- src/app/home/components/projetosUsuario.tsx
+++ src/app/home/components/projetosUsuario.tsx
@@ -53,7 +53,7 @@ const ProjectCards = ({ userId }: { userId: string }) => {
   const fetchParticipants = async (projectId: number) => {
     try {
       const responseUserType = await axiosInstance.get(`/users/${userId}`)
-      const url = await responseUserType.data.tipo_perfil !== "gestor" ? `/users/project/${projectId}` : "/projetos"
+      const url = "/projetos"
       console.log("tipo de usuário: ",responseUserType.data.tipo_perfil, ", url: ", url);
 
       const response = await axiosInstance.get(url);
