f685b952e955e4585bfa2339cf40571d17cb8fb3
paulooctavioINTELI <124174379+paulooctavioINTELI@users.noreply.github.com>
2025-02-25 13:43:45 -0300

    feat: turn login page into responsive page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -20,7 +20,7 @@ export default function LoginPage() {
 
   useEffect(() => {
     if (isSignedIn) {
-      window.location.href = "/home"; // Redireciona corretamente
+      window.location.href = "/home";
     }
   }, [isSignedIn]);
 
@@ -30,7 +30,7 @@ export default function LoginPage() {
       return response.data.tipo_perfil;
     } catch (err) {
       console.error("Erro ao buscar o tipo de perfil do usuário:", err);
-      return null; // Retorna null em caso de erro
+      return null;
     }
   }
 
@@ -51,35 +51,20 @@ export default function LoginPage() {
       });
 
       if (response.status === "complete") {
-        // Ativa a sessão
         await setActive({ session: response.createdSessionId });
-
-        // Obtém os detalhes do usuário autenticado
         const session = client.sessions.find(
           (session) => session.id === response.createdSessionId
         );
 
         if (session) {
           const userId = session.user?.id;
-
           if (userId) {
-            // Busca o tipo de usuário
             const userType = await fetchUserType(userId);
-
-            // Define o ID do usuário e o tipo de perfil nos cookies
-            setCookie(
-              "userId",
-              JSON.stringify({ id: userId, type: userType }),
-              {
-                httpOnly: false, // Configura como true para maior segurança
-                secure: true, // Somente envia o cookie em conexões HTTPS
-                maxAge: 60 * 60 * 24 * 30, // Duração de 30 dias
-                path: "/", // Disponível em todo o site
-              }
-            );
-            console.log("Usuário e tipo armazenados nos cookies:", {
-              id: userId,
-              type: userType,
+            setCookie("userId", JSON.stringify({ id: userId, type: userType }), {
+              httpOnly: false,
+              secure: true,
+              maxAge: 60 * 60 * 24 * 30,
+              path: "/",
             });
           } else {
             throw new Error("ID do usuário não encontrado na sessão.");
@@ -87,8 +72,6 @@ export default function LoginPage() {
         } else {
           throw new Error("Sessão não encontrada.");
         }
-
-        // Redirecionamento para outra página
         window.location.href = "/home";
       } else {
         setError("Autenticação incompleta. Verifique suas credenciais.");
@@ -101,23 +84,23 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex h-screen bg-[#141414]">
+    <div className="flex flex-col md:flex-row h-screen bg-[#141414]">
       <Image
         src={Mountains}
         className="w-full absolute bottom-0 z-0"
         alt="montain"
       />
-      <div className="flex flex-col justify-around items-center w-1/2 p-10 z-10 bg-[#1b1b1b] rounded-r-3xl shadow-2xl">
-        <Image src={LogoFull} className="w-60" alt="logo-full" />
-        <form className="flex flex-col gap-5 w-96" onSubmit={handleSubmit}>
+      <div className="flex flex-col justify-center items-center w-full md:w-1/2 p-6 md:p-10 z-10 bg-[#1b1b1b] rounded-none md:rounded-r-3xl shadow-2xl">
+        <Image src={LogoFull} className="w-40 md:w-60" alt="logo-full" />
+        <form className="flex flex-col gap-5 w-full max-w-sm" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-2">
-            <label htmlFor="login" className="text-xl font-fustat text-[#eeee]">
+            <label htmlFor="login" className="text-lg md:text-xl text-[#eeee]">
               Login
             </label>
             <input
               type="text"
               id="login"
-              className="px-5 text-black rounded-xl border shadow-inner outline-none bg-[#D9D8D8] h-[4rem]"
+              className="px-4 py-2 text-black rounded-xl border shadow-inner outline-none bg-[#D9D8D8]"
               value={identifier}
               onChange={(e) => setIdentifier(e.target.value)}
               required
@@ -125,27 +108,24 @@ export default function LoginPage() {
           </div>
 
           <div className="flex flex-col gap-2">
-            <label
-              htmlFor="password"
-              className="text-xl font-fustat text-[#eeee]"
-            >
+            <label htmlFor="password" className="text-lg md:text-xl text-[#eeee]">
               Senha
             </label>
             <input
               type="password"
               id="password"
-              className="px-5 text-black rounded-xl border shadow-inner outline-none bg-[#D9D8D8] h-[4rem]"
+              className="px-4 py-2 text-black rounded-xl border shadow-inner outline-none bg-[#D9D8D8]"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
           </div>
 
-          {error && <p className="text-red-500">{error}</p>}
+          {error && <p className="text-red-500 text-sm md:text-base">{error}</p>}
 
           <button
             type="submit"
-            className="mt-5 py-3 w-full bg-[#4DB8FF] text-white hover:bg-[#0070bb] font-fustat font-semibold transition-all rounded-xl text-lg"
+            className="mt-5 py-2 md:py-3 w-full bg-[#4DB8FF] text-white hover:bg-[#0070bb] font-semibold transition-all rounded-xl text-lg"
             disabled={isLoading}
           >
             {isLoading ? "Entrando..." : "Entrar"}
